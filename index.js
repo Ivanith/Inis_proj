f345bbeb976b62ce980ba45fb21ef0e1d13f9480
Ivanth <ivan.tkach.03@ukr.net>
2023-10-13 16:06:27 +0200

    lobby test

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ import { handleValidationErrors, checkAuth } from "./middleware/index.js";
 
 import { GameController, UserController } from "./controllers/index.js";
 
+import http from "http";
+
+import { Server } from "socket.io";
+
 mongoose
   .connect(
     "mongodb+srv://admin:qwerty123@cluster0.hsgvn1r.mongodb.net/Inis_proj?retryWrites=true&w=majority"
@@ -84,12 +88,90 @@ app.patch("/users/me/pass", checkAuth, UserController.updatePass);
 
 app.post("/game/create", checkAuth, GameController.create);
 
-//server check
 
-app.listen(4444, (err) => {
+
+//upd for sockets
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+
+
+//lobby part
+
+const lobbies = {};
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+//lobby list
+  function updateLobbyList() {
+    const lobbyList = Object.keys(lobbies).map((lobbyId) => ({
+      id: lobbyId,
+      name: lobbies[lobbyId].name,
+      players: lobbies[lobbyId].players.length,
+      maxPlayers: lobbies[lobbyId].maxPlayers,
+    }));
+    io.emit("lobby list", lobbyList);
+  }
+
+  socket.on("create lobby", (lobbyName, maxPlayers) => {
+    const lobbyId = generateRandomLobbyId();
+    lobbies[lobbyId] = {
+      name: lobbyName,
+      maxPlayers: maxPlayers,
+      players: [socket.id],
+      messages: [],
+    };
+    socket.join(lobbyId);
+    socket.emit("lobby created", lobbyId);
+    console.log(`Lobby created: ${lobbyId}`);
+  });
+
+  socket.on("join lobby", (lobbyId) => {
+    if (lobbies[lobbyId] && lobbies[lobbyId].players.length < lobbies[lobbyId].maxPlayers) {
+      lobbies[lobbyId].players.push(socket.id);
+      socket.join(lobbyId);
+      socket.emit("lobby joined", lobbyId);
+      console.log(`User joined Lobby: ${lobbyId}`);
+    } else {
+      socket.emit("lobby full");
+    }
+  });
+
+  socket.on("send message", (lobbyId, message) => {
+    if (lobbies[lobbyId]) {
+      const playerName = socket.id; // add users logic
+      const messageObject = { playerName, message };
+      lobbies[lobbyId].messages.push(messageObject);
+      io.to(lobbyId).emit("new message", messageObject);
+    }
+  });
+
+  socket.on("request lobby list", () => {
+    updateLobbyList();
+  });
+
+  socket.on("disconnect", () => {
+    //add more
+  });
+});
+
+function generateRandomLobbyId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+// server check
+const PORT = 4444;
+server.listen(PORT, (err) => {
   if (err) {
     return console.log(err);
   }
 
-  console.log("Server Ok");
+  console.log(`Server is running on port ${PORT}`);
 });
